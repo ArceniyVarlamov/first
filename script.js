7558a974e288cac99bbe81a64f3b091d02780b8f
Arceniy <arceniyvarlamov@gmail.com>
2022-07-26 14:51:10 +0500

    Revert "first_changes"
    This reverts commit 80b38962cec59c48ad35aeefe92b2ace4cd90347.

--- script.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector('.header__menu');
-
-menu.addEventListener('click', function f(event) {
-    let appear1 = document.querySelector('.findAppear').classList;
-    let appear2 = document.querySelector('.header__menu').classList;
-    appear1.toggle('header__appearance_active')
-    appear2.toggle('header__menu_active')
-})
